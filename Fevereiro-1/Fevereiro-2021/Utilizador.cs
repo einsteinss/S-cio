baee2b3e16a626058677e7a3056912dd5de7baaf
einsteinss <einsteinss23@gmail.com>
2021-01-26 16:28:49 +0000

    segunda tela

---
+++ Fevereiro-1/Fevereiro-2021/Utilizador.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sela Smith
+ * Date: 26/01/2021
+ * Time: 16:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fevereiro_2021
+{
+	/// <summary>
+	/// Description of Utilizador.
+	/// </summary>
+	public partial class Utilizador : Form
+	{
+		public Utilizador()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
