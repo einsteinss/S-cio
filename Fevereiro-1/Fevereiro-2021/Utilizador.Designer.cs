e507a3d207f730d2e6e890e86f32bf68e86f5ee9
einsteinss <einsteinss23@gmail.com>
2021-01-30 07:02:11 +0000

    update two forms 1-2

--- Fevereiro-1/Fevereiro-2021/Utilizador.Designer.cs
+++ Fevereiro-1/Fevereiro-2021/Utilizador.Designer.cs
@@ -14,6 +14,9 @@ namespace Fevereiro_2021
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -37,7 +40,44 @@ namespace Fevereiro_2021
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Utilizador));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.White;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 13.25F);
+			this.textBox1.Location = new System.Drawing.Point(73, 231);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(164, 44);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Arial", 10F);
+			this.label1.Location = new System.Drawing.Point(83, 254);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(147, 21);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Faça mais Compras";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Arial", 10F);
+			this.label2.Location = new System.Drawing.Point(56, 238);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(202, 21);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "__________________";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			// 
 			// Utilizador
 			// 
@@ -46,15 +86,18 @@ namespace Fevereiro_2021
 			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
 			this.ClientSize = new System.Drawing.Size(309, 549);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
 			this.DoubleBuffered = true;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
 			this.Name = "Utilizador";
 			this.ShowIcon = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Utilizador";
 			this.Load += new System.EventHandler(this.UtilizadorLoad);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 	}
